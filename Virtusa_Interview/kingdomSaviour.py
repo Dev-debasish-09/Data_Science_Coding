ebbd28012e79312f34af9d07d0698e21e6b95cb6
DEBASISH PRADHAN <161795696+Dev-debasish-09@users.noreply.github.com>
2025-11-04 14:38:32 +0530

    Create kingdomSaviour.py

---
+++ Virtusa_Interview/kingdomSaviour.py
@@ -0,0 +1,53 @@
+# Read only region start
+class UserMainCode(object):
+    @classmethod
+    def kingdomSaviour(cls, input1, input2, input3, input4, input5):
+        """
+        input1 : int -> initial power of the king
+        input2 : int -> number of monsters
+        input3 : List[int] -> type of each monster (0 or 1)
+        input4 : List[int] -> health of each monster
+        input5 : List[int] -> increment in king's power after killing each monster
+        
+        Expected return type : int
+        """
+        # Read only region end
+        
+        # --- Write code here ---
+        
+        # Separate monsters by type 0 and 1
+        monsters0 = sorted([(input4[i], input5[i]) for i in range(input2) if input3[i] == 0])
+        monsters1 = sorted([(input4[i], input5[i]) for i in range(input2) if input3[i] == 1])
+        
+        # Simulation function
+        def simulate(start_type):
+            power = input1
+            t = start_type
+            i0 = i1 = 0
+            count = 0
+            
+            while True:
+                if t == 0:
+                    # Find next killable monster of type 0
+                    while i0 < len(monsters0) and monsters0[i0][0] > power:
+                        i0 += 1
+                    if i0 == len(monsters0):
+                        break
+                    power += monsters0[i0][1]
+                    i0 += 1
+                    t = 1
+                    count += 1
+                else:
+                    # Find next killable monster of type 1
+                    while i1 < len(monsters1) and monsters1[i1][0] > power:
+                        i1 += 1
+                    if i1 == len(monsters1):
+                        break
+                    power += monsters1[i1][1]
+                    i1 += 1
+                    t = 0
+                    count += 1
+            return count
+        
+        # Try starting with both types and take the maximum
+        return max(simulate(0), simulate(1))
