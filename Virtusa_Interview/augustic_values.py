8caadac247078faf7860071ea13684ff17e325e8
DEBASISH PRADHAN <161795696+Dev-debasish-09@users.noreply.github.com>
2025-11-04 14:21:41 +0530

    Update augustic_values.py

--- Virtusa_Interview/augustic_values.py
+++ Virtusa_Interview/augustic_values.py
@@ -19,3 +19,30 @@ class UserMainCode(object):
 
 obj = UserMainCode()
 print(obj.augustic_values('125'))
+
+###########################################################################################################
+
+class UserMainCode(object):
+    @classmethod
+    def agusticValues(cls, input1: str) -> str:
+        
+        def evaluate_expression(expr):
+            # Split at '+' and sum the integer parts
+            return sum(int(num) for num in expr.split('+'))
+        
+        def helper(s, n, index, current_expr):
+            # Base condition: reached the last character
+            if index == n - 1:
+                current_expr += s[index]
+                return evaluate_expression(current_expr)
+            
+            # Case 1: without inserting '+'
+            without_plus = helper(s, n, index + 1, current_expr + s[index])
+            
+            # Case 2: inserting '+'
+            with_plus = helper(s, n, index + 1, current_expr + s[index] + '+')
+            
+            return without_plus + with_plus
+        
+        return str(helper(input1, len(input1), 0, ""))
+
