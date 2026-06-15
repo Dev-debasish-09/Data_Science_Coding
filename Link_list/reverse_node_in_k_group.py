be3d6413b0800d4a04e068f125606a3f73ee0545
DEBASISH PRADHAN <161795696+Dev-debasish-09@users.noreply.github.com>
2026-06-15 20:55:38 +0530

    Create reverse_node_in_k_group.py

---
+++ Link_list/reverse_node_in_k_group.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        curr = head
+        count = 0
+
+        # Check if k nodes exist
+        while curr and count < k:
+            curr = curr.next
+            count += 1
+
+        if count < k:
+            return head
+
+        # Reverse first k nodes
+        prev = None
+        curr = head
+
+        for _ in range(k):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # Connect remaining list
+        head.next = self.reverseKGroup(curr, k)
+
+        return prev
